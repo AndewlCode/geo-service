4fc88e5104d496407db5c9b51a9dab478b2c7183
Andrey Lobachev <andrey_lobachev@internet.ru>
2023-04-25 21:14:27 +0300

    MessageSenderTest edited

--- src/test/java/ru/netology/sender/MessageSenderTest.java
+++ src/test/java/ru/netology/sender/MessageSenderTest.java
@@ -1,2 +1,37 @@
-package ru.netology.sender;public class MessageSenderTest {
+package ru.netology.sender;
+
+import org.mockito.Mockito;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MessageSenderTest {
+    @Test
+    void messageSenderTestRussianIP() {
+        MessageSender messageSender = Mockito.mock(MessageSender.class);
+        Map<String, String> headers = new HashMap<>();
+        Mockito.when(messageSender.send(headers)).thenReturn("Русский", "172");
+
+        String expected = "Русский";
+        String message = messageSender.send(headers);
+
+        Assertions.assertEquals(expected, message);
+
+    }
+
+    @Test
+    void messageSenderTestNotRussianIP() {
+        MessageSender messageSender = Mockito.mock(MessageSender.class);
+        Map<String, String> headers = new HashMap<>();
+        Mockito.when(messageSender.send(headers)).thenReturn("English", "78");
+
+        String expected = "English";
+        String message = messageSender.send(headers);
+
+        Assertions.assertEquals(expected, message);
+
+    }
 }
