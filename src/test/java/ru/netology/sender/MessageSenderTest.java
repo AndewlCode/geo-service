ea81fdb2fcd12d31df61a404f2a8b713cc28a443
Andrey Lobachev <lobatchev.andrew@gmail.com>
2023-04-27 09:58:30 +0300

    jUnit modules updated

--- src/test/java/ru/netology/sender/MessageSenderTest.java
+++ src/test/java/ru/netology/sender/MessageSenderTest.java
@@ -1,5 +1,4 @@
 package ru.netology.sender;
-
 import org.mockito.Mockito;
 
 import java.util.HashMap;
@@ -7,17 +6,16 @@ import java.util.Map;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import ru.netology.MessageSender;
 
 public class MessageSenderTest {
     @Test
     void messageSenderTestRussianIP() {
         MessageSender messageSender = Mockito.mock(MessageSender.class);
-        Map<String, String> headers = new HashMap<>();
+        Map<String, String> headers = new HashMap<String, String>();
         Mockito.when(messageSender.send(headers)).thenReturn("Русский", "172");
-
         String expected = "Русский";
         String message = messageSender.send(headers);
-
         Assertions.assertEquals(expected, message);
 
     }
@@ -25,12 +23,10 @@ public class MessageSenderTest {
     @Test
     void messageSenderTestNotRussianIP() {
         MessageSender messageSender = Mockito.mock(MessageSender.class);
-        Map<String, String> headers = new HashMap<>();
+        Map<String, String> headers = new HashMap<String, String>();
         Mockito.when(messageSender.send(headers)).thenReturn("English", "78");
-
         String expected = "English";
         String message = messageSender.send(headers);
-
         Assertions.assertEquals(expected, message);
 
     }
