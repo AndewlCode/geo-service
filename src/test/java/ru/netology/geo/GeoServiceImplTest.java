f65d9e2606305c2b75e59910bf5d7c7a9f10eaee
Andrey Lobachev <lobatchev.andrew@gmail.com>
2023-04-27 16:38:45 +0300

    GeoServiceImplTest added

--- src/test/java/ru/netology/geo/GeoServiceImplTest.java
+++ src/test/java/ru/netology/geo/GeoServiceImplTest.java
@@ -1,16 +1,31 @@
 package ru.netology.geo;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import ru.netology.entity.Country;
+import ru.netology.entity.Location;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GeoServiceImplTest {
 
     @Test
-    void byIp() {
+    void byIpFromMoscow() {
+        GeoServiceImpl geoService = Mockito.spy(GeoServiceImpl.class);
+        Location expected = new Location("Moscow", Country.RUSSIA, "Lenina", 15);
+        Location location = geoService.byIp(GeoServiceImpl.MOSCOW_IP);
+
+        assert expected.getCity().equals(location.getCity());
+    }
+    @Test
+    void byIpFromNewYork(){
+        GeoServiceImpl geoService = Mockito.spy(GeoServiceImpl.class);
+        Location expected = new Location("New York", Country.USA, " 10th Avenue", 32);
+        Location location = geoService.byIp(GeoServiceImpl.NEW_YORK_IP);
+
+        assert expected.getCity().equals(location.getCity());
     }
 
-    @Test
-    void byCoordinates() {
-    }
+
 }
